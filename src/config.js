2bace211031803aecbac48981efca275329c9a03
mhdareeb <areeb.mohammad11@gmail.com>
2025-04-07 00:25:04 -0500

    correct base_url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = 
 {
-  "base_url": "https://git.heroku.com/floating-wildwood-32792.git"
+  "base_url": "https://floating-wildwood-32792-afb0e7b55a21.herokuapp.com"
 }
 
 export default config;
